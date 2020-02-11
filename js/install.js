290ad1bbefd1c48f21fa0c3c0da023ee9004d7cb
Mario-Leonel <50590430+Mario-Leonel@users.noreply.github.com>
2020-02-10 22:34:57 -0600

    Update install.js

--- js/install.js
+++ js/install.js
@@ -4,28 +4,28 @@ let logAppInstalled;
 
 window.addEventListener("beforeinstallprompt", e => {
   // Stash the event so it can be triggered later.
+  e.preventDefault();
   deferredPrompt = e;
+  btnAdd.style.display = 'block';
   // Actualizar UI notifica al usuario que puede agregar a la pantalla de inicio
   
 });
-window.addEventListener("click", e => {
+btnAdd.addEventListener('click', (e) => {
   // hide our user interface that shows our A2HS button
-  window.style.display = "none";
+  //btnAdd.style.display = 'none';
   // Show the prompt
   deferredPrompt.prompt();
   // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then(choiceResult => {
-    if (choiceResult.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt");
-    } else {
-      console.log("User dismissed the A2HS prompt");
-    }
-    deferredPrompt = null;
-  });
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } 
+      deferredPrompt = null;
+    });
 });
 
-// CODELAB: Add event listener for appinstalled event
-window.addEventListener("appinstalled", logAppInstalled);
-// CODELAB: Add code to log the event
-//console.log("Weather App was installed.", evt);
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs installed');
+});
 
